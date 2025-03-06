8a308b4ed38e0bd8f551f94e9fe8c1f56825a679
Dark Web Informer <162650054+DarkWebInformer@users.noreply.github.com>
2025-03-06 18:58:19 +0000

    Added
    "ns1.usssdomainseizure.com", "ns2.usssdomainseizure.com"

--- fbi_watchdog.py
+++ fbi_watchdog.py
@@ -109,7 +109,7 @@ ascii_banner = r"""
      _,-' ,'`-__; '--.
     (_/'~~      ''''(;                                                    
 
-[bold blue]FBI Watchdog v2.0.1 by [link=https://darkwebinformer.com]Dark Web Informer[/link][/bold blue]
+[bold blue]FBI Watchdog v2.0.2 by [link=https://darkwebinformer.com]Dark Web Informer[/link][/bold blue]
 """
 
 console.print(Padding(f"[bold blue]{ascii_banner}[/bold blue]", (0, 0, 0, 4)))
